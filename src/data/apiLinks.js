b4076f54db9e3f50d6386b75a37f670958a6d5c5
Tito <thiagorobles3@gmail.com>
2025-05-02 19:30:09 -0300

    Cambiando detalles y refactorizando la pagina

---
+++ src/data/apiLinks.js
@@ -0,0 +1,13 @@
+//Imagenes de APIs
+import catImg from "../assets/gatos.jpg";
+import pokeImg from "../assets/pokemon.jpg";
+import rickmortyImg from "../assets/rickandmorty.jpg"; // Importa la imagen de Rick y Morty
+
+
+
+export const apiLinks = [
+   
+    { img: pokeImg, alt: "PokeAPI", route: "/poke" },
+    { img: catImg, alt: "Cat API", route: "/cat" },
+    { img: rickmortyImg, alt: "Rick and Morty API", route: "/rickmorty" }
+  ];
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,21 +1,43 @@
-import React from 'react';
-import './styles/HomePage.css'; // Importa el archivo CSS para estilos adicionales
-import rickmortyImg from '../assets/rickandmorty.jpg'; // Importa la imagen de Rick y Morty
+import React from "react";
+import "./styles/HomePage.css"; // Importa el archivo CSS para estilos adicionales
+
+import { apiLinks } from "../data/apiLinks";
+
+//Hooks
+import { useNavigate } from "react-router-dom";
+
+
 
 export const HomePage = () => {
-    return (
-        <div className="home-container">
+  const navigate = useNavigate(); // Hook para la navegación
 
-            <img src={rickmortyImg} alt="Rick and Morty" className="profile-pic" />
+    
+  return (
+    <div className="home-container">
+      <div className="api-icons-container">
+        {apiLinks.map(({ img, alt, route }, index) => (
+          <img
+            key={index}
+            src={img}
+            alt={alt}
+            className="profile-pic"
+            onClick={() => navigate(route)}
+            style={{ cursor: "pointer" }}
+            title={alt}
+          />
+        ))}
+      </div>
 
-            <h1 className="home-title">¡Bienvenido a The World API!</h1>
-            <p className="home-subtitle">
-                Explora un mundo de datos fascinantes de múltiples APIs en un solo lugar.
-            </p>
-            <p className="home-description">
-                Este proyecto combina la magia de laa APIs para ofrecerte una experiencia única. 
-                ¡Selecciona una opción del menú y comienza tu aventura!
-            </p>
-        </div>
-    );
+      <h1 className="home-title">¡Bienvenido a The World API!</h1>
+      <p className="home-subtitle">
+        Explora un mundo de datos fascinantes de múltiples APIs en un solo
+        lugar.
+      </p>
+      <p className="home-description">
+        Este proyecto combina la magia de laa APIs para ofrecerte una
+        experiencia única. ¡Selecciona una opción del menú y comienza tu
+        aventura!
+      </p>
+    </div>
+  );
 };
